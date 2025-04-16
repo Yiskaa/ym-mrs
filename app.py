d2d069e447b4b9ac45527e3af4987c8e32c2de83
Ayush <yuukokuuki2906@gmail.com>
2025-04-16 16:01:15 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+movies = pickle.load(open('C:\\Dump\\gcem\\py\\ym-mrs\\movie_list.pkl', 'rb'))
+similarity = pickle.load(open('C:\\Dump\\gcem\\py\\ym-mrs\\similarity.pkl', 'rb'))
+
+def recommend(movie):
+    try:
+        index = movies[movies['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
+        recommended_movie_names = []
+        recommended_movie_posters = []
+        for i in distances[1:6]:  # Get top 5 recommendations
+            recommended_movie_names.append(movies.iloc[i[0]].title)
+            recommended_movie_posters.append(movies.iloc[i[0]].poster_links)
+        
+        return recommended_movie_names, recommended_movie_posters
+    except IndexError:
+        return [], []
+
+@app.route("/")
+def index():
+    return render_template("index.html", movie_list=movies['title'].values)
+
+@app.route("/recommend", methods=["POST"])
+def recommend_movies():
+    movie = request.json.get("movie")  # Get movie from JSON
+    recommended_names, recommended_posters = recommend(movie)
+
+    return jsonify({
+        'recommended_names': recommended_names,
+        'recommended_posters': recommended_posters
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ import pickle.py
@@ -0,0 +1,5 @@
+import pickle
+import pandas as pd
+import os
+
+movies = pickle.load(open('D:\\ym-mrs\\movie_list.pkl', 'rb'))
